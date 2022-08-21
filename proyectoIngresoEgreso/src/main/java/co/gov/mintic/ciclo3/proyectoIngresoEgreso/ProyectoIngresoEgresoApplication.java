5a31ceec303f678647ef83be8e940287b68f37ba
Javier Diaz <jaedgodi20@gmail.com>
2022-08-21 17:46:58 -0500

    Proyecto Ingreso Egreso

---
+++ proyectoIngresoEgreso/src/main/java/co/gov/mintic/ciclo3/proyectoIngresoEgreso/ProyectoIngresoEgresoApplication.java
@@ -0,0 +1,13 @@
+package co.gov.mintic.ciclo3.proyectoIngresoEgreso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIngresoEgresoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIngresoEgresoApplication.class, args);
+	}
+
+}
