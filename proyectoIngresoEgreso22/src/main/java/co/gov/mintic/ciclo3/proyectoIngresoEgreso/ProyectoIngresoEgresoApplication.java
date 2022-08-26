f1b31d2067eb6349d0af4591452761062ce36e47
Diego Cetina <diegocetina@MacBook-Pro-de-Diego.local>
2022-08-26 09:14:16 -0600

    v1 del menu para cambiar los datos brindados por el usuario

--- proyectoIngresoEgreso22/src/main/java/co/gov/mintic/ciclo3/proyectoIngresoEgreso/ProyectoIngresoEgresoApplication.java
+++ proyectoIngresoEgreso22/src/main/java/co/gov/mintic/ciclo3/proyectoIngresoEgreso/ProyectoIngresoEgresoApplication.java
@@ -52,6 +52,61 @@ public class ProyectoIngresoEgresoApplication {
 
 		}
 
+		int opcion;
+		Scanner seleccion_1 = new Scanner(System.in);
+		System.out.println("DESEA REALIZAR ALGUN CAMBIO A LA INFORMACION ADICIONADA\n"
+				+ "1. SI\n"
+				+ "2. NO\n");
+		 opcion= seleccion_1.nextInt();
+
+		switch (opcion) {
+			case 1:
+				int sel;
+				Scanner seleccion_2 = new Scanner(System.in);
+				System.out.println("DONDE DESEA GENERAR LOS CAMBIOS?\n"
+						+ "1. EMPRESA\n"
+						+ "2. EMPLEADO\n"
+						+  "3.MOVIMIENTOS");
+				sel= seleccion_2.nextInt();
+				switch (dato) {
+					case 1:
+						empresa datos = new empresa();
+						datos.setNombreEmpresa();
+						datos.setDireccion();
+						datos.setTelefono();
+						datos.setNIT();
+						datos.setIdAdministrador();
+						System.out.println(datos.toString());
+						break;
+					case 2:
+						empleado employed = new empleado();
+						employed.setNombreEmpleado();
+						employed.setCorreo();
+						employed.setEmpresaPerteneciente();
+						employed.setIdEmpleado();
+						System.out.println(employed.toString());
+						break;
+					case 3:
+						movimientoDinero dinero = new movimientoDinero();
+						dinero.setMontoMovimiento();
+						dinero.setMontoPositivo();
+						dinero.setMontoNegativo();
+						dinero.setIdEmpleadoMovimiento();
+						System.out.println(dinero.toString());
+						break;
+					default:
+						System.out.println("opcion no valida reincie el programa");
+						break;
+				}
+
+				break;
+			case 2:
+				System.out.println("GRACIAS POR SE PARTE DE NUESTA EMPRESA");
+
+			default:
+				System.out.println("opcion no valida reincie el programa");
+				break;
+		}
 	}
 
 }
